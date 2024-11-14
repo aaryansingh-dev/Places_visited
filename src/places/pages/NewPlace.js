a3d55c56a38d9cb36fc372b28d2db6eadd1ce203
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-11-13 18:10:46 -0700

    Basic Input field created

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const NewPlace =() =>{
-    return (
-        <h2>Place works</h2>
-    );
-}
+import './NewPlace.css'
+import Input from '../../shared/components/FormElements/Input';
 
+
+ const NewPlace = props => {
+    return (
+        <form className='place-form'>
+            <Input element="input" type="text" label="Title" validators={[]} errorText="Please enter a valid Title"/>
+        </form>
+    )
+ }
 export default NewPlace;
